7fac3247e9a8acfb35887c6e3f7262ed5d76dc15
denabPK <denabpavithran@gmail.com>
2024-10-14 09:44:54 +0530

    Add files via upload

---
+++ 4.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter the value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",i);
+	}
+	for(i=1;i<=n;n--)
+	{
+		printf("%d\t",n);
+	}
+	return 0;
+	
+	
+}
