07aafaf77c7bcbddfd206570782a469b3599b65d
denabPK <denabpavithran@gmail.com>
2024-11-24 22:19:24 +0530

    Add files via upload

---
+++ max.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,max;
+	printf("enter the first number :");
+	scanf("%d",&a);
+	printf("enter the second number :");
+	scanf("%d",&b);
+	if(a<b)
+	{
+		printf("b is max");
+	}
+	else
+	{
+		printf("a is max");
+	}
+	
+	return 0;
+}
